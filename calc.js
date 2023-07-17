e8fdea7dadfd5f4304a920ca81ba97d1458c413d
unknown <snehaprakash611@gmail.com>
2023-07-17 15:49:07 +0530

    modify

---
+++ calc.js
@@ -0,0 +1,12 @@
+function displayData(n){
+    result.value+=n
+}
+function evaluateEx(n){
+    result.value=eval(result.value)
+}
+function Del(){
+    result.value=result.value.slice(0,-1)
+}
+function allClear(){
+    result.value=' '
+}
